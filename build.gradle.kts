7beaf10658c1d47d179642d34a2384d6e2f1039f
WardLordRuby <chad2lax@gmail.com>
2026-02-09 15:52:33 -0600

    Add spotless dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,20 @@
 plugins {
     id("java")
+    id("com.diffplug.spotless") version "6.25.0"
 }
 
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(25))
+    }
+}
+
+spotless {
+    java {
+        removeUnusedImports()
+        importOrder("com.wardlordruby", "com.hypixel", "com", "java", "javax")
+        trimTrailingWhitespace()
+        endWithNewline()
     }
 }
 
@@ -19,6 +29,10 @@ dependencies {
     compileOnly(files("libs/HytaleServer.jar"))
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.build {
+    dependsOn(tasks.spotlessApply)
 }
 
 tasks.test {
